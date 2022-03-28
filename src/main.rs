bafba49e6f7c52fb8f4006ced251c550a9ce15b6
Palash Bauri <palashbauri1@gmail.com>
2022-03-28 17:51:22 +0530

    fixed [kind of] the tab function in textarea

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use cursive::event::{Event, Key};
-use cursive::views::{Button, Dialog, DummyView, EditView, LinearLayout, TextArea};
+use cursive::views::{
+    Button, Dialog, DummyView, EditView, LinearLayout, TextArea, ViewRef,
+};
 use cursive::{theme, traits::*};
 
 fn main() {
@@ -14,10 +16,20 @@ fn main() {
         theme.palette[theme::PaletteColor::Secondary] = theme::Color::parse("#3a3a3a").unwrap();
     });
 
-    let tx = cursive::views::ThemedView::new(
+    let tx = cursive::views::OnEventView::new(cursive::views::ThemedView::new(
         my_theme.clone(),
         TextArea::new().with_name("tsi").full_width().full_height(),
-    );
+    ))
+    .on_event(Event::Key(Key::Tab), |s| {
+        let mut tt: ViewRef<TextArea> = s.find_name("tsi").unwrap();
+        let mut prev_content = tt.get_content().to_owned();
+        prev_content.push_str("    ");
+        tt.set_content(prev_content.clone());
+        //let c_cursor = tt.cursor();
+        tt.set_cursor(prev_content.len());
+        //tt.set_cursor(prev_content.len())
+        //s.add_layer(Dialog::new().content(TextView::new(tt.cursor().to_string())).dismiss_button("close"));
+    });
 
     let rx =
         cursive::views::ThemedView::new(my_theme, EditView::new().with_name("tri").full_width());
