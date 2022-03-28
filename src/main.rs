85a9933845f5e075ee9b882d41dc7ad3962f19f1
Palash Bauri <palashbauri1@gmail.com>
2022-03-28 08:50:10 +0530

    basic ui prototype

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
-//use std::collections::hash_map::Keys;
+use cursive::event::{Event, Key};
 use cursive::views::{Button, Dialog, DummyView, EditView, LinearLayout, TextArea};
 use cursive::{theme, traits::*};
 
 fn main() {
     let mut siv = cursive::default();
-    siv.load_theme_file("/home/palash/arx/themes/dark.toml")
-        .unwrap();
+
+    //experimental theme;
+
+    siv.load_theme_file("themes/dark.toml").unwrap();
+    siv.clear_global_callbacks(Event::Key(Key::Tab));
     let my_theme = siv.current_theme().clone().with(|theme| {
         theme.palette[theme::PaletteColor::View] = theme::Color::Light(theme::BaseColor::White);
         theme.palette[theme::PaletteColor::Secondary] = theme::Color::parse("#3a3a3a").unwrap();
@@ -15,7 +18,6 @@ fn main() {
         my_theme.clone(),
         TextArea::new().with_name("tsi").full_width().full_height(),
     );
-    //Selector::Name("tsi")
 
     let rx =
         cursive::views::ThemedView::new(my_theme, EditView::new().with_name("tri").full_width());
@@ -30,15 +32,7 @@ fn main() {
                     .full_width(),
             )
             .child(DummyView)
-            .child(
-                LinearLayout::horizontal()
-                    /*.child(TextArea::new()
-                    .with_name("string_input")
-                    .full_width()
-                    .full_height())
-                    */
-                    .child(tx),
-            ),
+            .child(LinearLayout::horizontal().child(tx)),
     ));
     siv.run();
 }
